f63f11dd66d4314a0141ef42efac3e63b9391fd6
LaercioCosta <laerciocostam1999@gmail.com>
2021-09-21 05:19:59 -0300

    Projeto TCC

---
+++ JS/firebase.js
@@ -0,0 +1,24 @@
+      import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+      import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+      import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
+
+      // https://firebase.google.com/docs/web/setup#available-libraries
+
+      const firebaseConfig = {
+          apiKey: "AIzaSyCdEDWI7kcymn091j5TkwGKrOxb-zZRuqI",
+          authDomain: "meu-aconchego.firebaseapp.com",
+          projectId: "meu-aconchego",
+          storageBucket: "meu-aconchego.appspot.com",
+          messagingSenderId: "300674730016",
+          appId: "1:300674730016:web:a43887c025abe28f2bd445",
+          measurementId: "G-JJ9DVX72KH"
+      };
+
+      // Initialize Firebase
+      const firebase = initializeApp(firebaseConfig);
+      
+    
+      export { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+      
+
+
