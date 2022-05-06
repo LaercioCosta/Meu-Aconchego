d36c9894f4d345d26c923dd4119b283aab635ca1
Laércio Costa <laerciocostam1999@gmail.com>
2022-05-06 11:53:24 -0300

    Update firebase.js

--- JS/firebase.js
+++ JS/firebase.js
@@ -5,13 +5,13 @@
       // https://firebase.google.com/docs/web/setup#available-libraries
 
       const firebaseConfig = {
-          apiKey: "AIzaSyCdEDWI7kcymn091j5TkwGKrOxb-zZRuqI",
-          authDomain: "meu-aconchego.firebaseapp.com",
-          projectId: "meu-aconchego",
-          storageBucket: "meu-aconchego.appspot.com",
-          messagingSenderId: "300674730016",
-          appId: "1:300674730016:web:a43887c025abe28f2bd445",
-          measurementId: "G-JJ9DVX72KH"
+          apiKey: 
+          authDomain: 
+          projectId: 
+          storageBucket: 
+          messagingSenderId: 
+          appId: 
+          measurementId:
       };
 
       // Initialize Firebase
